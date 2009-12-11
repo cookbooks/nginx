1d95c95f87c6bc98ea634946d9e30c91c5e0fc44
Joshua Sierles <joshua@rasgueado.local>
2009-12-11 01:13:17 +0100

    finish move of recipes to top level

---
+++ attributes/nginx.rb
@@ -0,0 +1,28 @@
+default.nginx[:version] = "0.8.29-1"
+default.nginx[:architecture] = kernel[:machine] == "x86_64" ? "amd64" : "i386"
+default.nginx[:dir]     = "/etc/nginx"
+default.nginx[:log_dir] = "/var/log/nginx"
+default.nginx[:user]    = "www-data"
+default.nginx[:group]   = "www-data"
+default.nginx[:binary]  = "/usr/sbin/nginx"
+
+default.nginx[:gzip] = "on"
+default.nginx[:gzip_http_version] = "1.0"
+default.nginx[:gzip_comp_level] = "2"
+default.nginx[:gzip_proxied] = "any"
+default.nginx[:gzip_types] = [ "text/plain", "text/html", "text/css", "application/javascript", "application/x-javascript", "text/xml", "application/xml", "application/xml+rss", "text/javascript" ]
+
+default.nginx[:keepalive] = "on"
+default.nginx[:keepalive_timeout] = 3
+
+default.nginx[:worker_processes] = 6
+default.nginx[:worker_connections] = 2048
+default.nginx[:server_names_hash_bucket_size] = 128
+
+default.nginx[:expires] = Mash.new
+default.nginx[:expires][:enabled] = true
+default.nginx[:expires][:regex] = '\.(ico|css|js|gif|jp?g|png)(\?[0-9]+)?'
+default.nginx[:expires][:time] = "max"
+
+default.nginx[:extras] = ['headers']
+default.nginx[:helpers] = ['expires', 'lb_filter']
---
+++ recipes/default.rb
@@ -0,0 +1,60 @@
+nginx_filename = ["nginx", node[:nginx][:version], node[:nginx][:architecture]].join("_")+".deb"
+
+package "libxslt1.1"
+package "libgeoip1"
+
+remote_file "/tmp/#{nginx_filename}" do
+  source nginx_filename
+end
+
+dpkg_package "nginx" do
+  source "/tmp/#{nginx_filename}"
+end
+
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+end
+
+directory node[:nginx][:log_dir] do
+  mode 0755
+  owner node[:nginx][:user]
+  action :create
+end
+
+%w{nxensite nxdissite}.each do |nxscript|
+  template "/usr/sbin/#{nxscript}" do
+    source "#{nxscript}.erb"
+    mode 0755
+    owner "root"
+    group "root"
+  end
+end
+
+template "nginx.conf" do
+  path "#{node[:nginx][:dir]}/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, resources(:service => "nginx")
+end
+
+directory "/etc/nginx/helpers"
+
+# helpers to be included in your vhosts
+node[:nginx][:helpers].each do |h|
+  template "/etc/nginx/helpers/#{h}.conf" do
+    notifies :reload, resources(:service => "nginx")
+  end
+end
+
+# server-wide defaults, automatically loaded
+node[:nginx][:extras].each do |ex|
+  template "/etc/nginx/conf.d/#{ex}.conf" do
+    notifies :reload, resources(:service => "nginx")
+  end
+end  
+
+service "nginx" do
+  action [ :enable, :start ]
+end
